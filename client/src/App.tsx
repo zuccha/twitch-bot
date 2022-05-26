ebd163f1f7e97abcc46655a7be54e6df695cd3c9
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2022-05-26 14:25:06 +0200

    feat(client): add home route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import AdminRoute from "./routes/quiz/QuizAdminRoute";
-import MainRoute from "./routes/MainRoute";
+import HomeRoute from "./routes/HomeRoute";
+import QuizAdminRoute from "./routes/quiz/QuizAdminRoute";
+import QuizMainRoute from "./routes/quiz/QuizMainRoute";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/admin" element={<AdminRoute />} />
-        <Route path="/" element={<MainRoute />} />
+        <Route path="/quiz/admin" element={<QuizAdminRoute />} />
+        <Route path="/quiz" element={<QuizMainRoute />} />
+
+        <Route path="/" element={<HomeRoute />} />
       </Routes>
     </BrowserRouter>
   );
