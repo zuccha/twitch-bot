58271b9e32b8d416353133a44ea7f0b4a5d2731e
Amedeo Zucchetti <amedeo.zucchetti@gmail.com>
2022-05-26 14:41:18 +0200

    feat(client): add chakra ui

--- client/src/routes/HomeRoute.tsx
+++ client/src/routes/HomeRoute.tsx
@@ -1,5 +1,17 @@
+import { Button, Flex, SimpleGrid } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 export default function HomeRoute() {
-  return <Link to="/quiz">Quiz</Link>;
+  return (
+    <Flex alignItems="center" justifyContent="center" height="100vh">
+      <SimpleGrid columns={2} spacing={6}>
+        <Button as={Link} to="/quiz">
+          Quiz
+        </Button>
+        <Button as={Link} to="/quiz/admin">
+          Quiz Admin
+        </Button>
+      </SimpleGrid>
+    </Flex>
+  );
 }
